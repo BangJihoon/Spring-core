5f94b435b299073402284a6295a31714e6db4fc2
bang jihoon <jihoon289@naver.com>
2022-02-23 17:25:14 +0900

    핵심 원리 7  - 롬복 추가 및 @Primary로 충돌제거

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,9 +2,11 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class FixDiscountPolicy implements DiscountPolicy{
     private final int discountFixAmount = 1000; // 고정할인 금액
 
