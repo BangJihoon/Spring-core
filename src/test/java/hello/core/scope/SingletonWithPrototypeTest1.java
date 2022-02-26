a8c1607629daaae555ebde8554591d88ce1dff92
Bang(home) <jihoon289@naver.com>
2022-02-27 01:53:32 +0900

    핵심 원리 9 - Web Scope 예제 추가

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -18,7 +18,7 @@ public class SingletonWithPrototypeTest1 {
         int count2 = clientBean2.logic();
         assertThat(count2).isEqualTo(2);
     }
-    // 싱글톤 빈에서 프로토타입 빈을 생성해주면, 정상 동작
+    // 싱글톤을 이용한 프로토타입
     static class ClientBean {
         private final PrototypeBean prototypeBean;
         @Autowired
